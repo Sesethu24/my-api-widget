0075f15b1f483c160d47e25ec705131348a578f7
Sesethu <seshmomoza20@gmail.com>
2022-03-29 14:45:50 +0200

    added some logic

---
+++ index.js
@@ -0,0 +1,22 @@
+const makesTemplateSource = document.querySelector(".makesTemplate")
+const makesTemplate = Handlebars.compile(makesTemplateSource.innerHTML)
+
+axios.get('http://api-tutor.herokuapp.com/v1/colors')
+
+.then(function(response){
+        console.log(response.data);
+        const colorsElem = document.querySelector(".colors")
+        colorsElem.innerHTML = makesTemplate({
+            makes: response.data
+        })
+ });
+
+ axios.get('http://api-tutor.herokuapp.com/v1/makes')
+
+ .then(function(response){
+    console.log(response.data);
+    const makesElem = document.querySelector(".brands")
+    makesElem.innerHTML = makesTemplate({
+        makes: response.data
+    })
+ });
