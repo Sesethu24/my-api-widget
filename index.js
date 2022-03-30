478afffe72c03a2347c488b1a361e9884ac52293
Sesethu <seshmomoza20@gmail.com>
2022-03-30 17:14:44 +0200

    added the filter function

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ const makesTemplateSource = document.querySelector(".makesTemplate")
 const makesTemplate = Handlebars.compile(makesTemplateSource.innerHTML)
 const carsElem = document.querySelector('.model');
 
+const colorElem = document.querySelector(".selectColor");
+const brandElem = document.querySelector(".selectBrand");
+const filterButton = document.querySelector(".filterBtn");
+const displayElem = document.querySelector(".display");
+
+const carFilterTemplateElem = document.querySelector(".carFilterTemplate");
+const carFilterTemplate = Handlebars.compile(carFilterTemplateElem.innerHTML);
+var filteredCars = [];
+
+     filterButton.addEventListener('click', function () {
+          var make = brandElem.value
+          var  color = colorElem.value
+
+        axios.get("http://api-tutor.herokuapp.com/v1/cars/make/" + make + "/color/" + color)
+         .then(function (result) {
+             console.log(result.data)
+           let data = result.data;
+                filteredCars.push(data)
+
+            console.log(filteredCars)
+             displayElem.innerHTML = carFilterTemplate({
+                 filteredCars : data
+             })
+         }) 
+     })
+
+
 axios.get('https://api-tutor.herokuapp.com/v1/colors')
 
 .then(function(response){
